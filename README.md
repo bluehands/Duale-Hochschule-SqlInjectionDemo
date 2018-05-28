44b3f844c70fdf25127a87fb3b1af9cd0028737d
Aydin Mir Mohammadi <am@bluehands.de>
2018-05-28 21:53:39 +0200

    Add Sample code and database create scripts to start the demo

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # SqlInjectionDemo
 A simple app to show how SqlInjection works. 
+
+## How to start
+* Create a MS Sql Server Database. See **CreateDB.txt** or use **SqlInjectionDemo.dacpac**
+* Don't forget to add soma data
+* Open the solution and adjust the connection string
+* Start the app
+
+## Demo
+Enter UserName and Password. The app just concat the sql string with the user input. DON'T DO THIS AT HOME. The resulting string is printed out for demonstration reason.
+
+The sql is executed against the database with Command.ExecuteReader and the result is printed. Use the sample input from **SampleSQL.txt** 
+
+Have fun.
+
+
---
+++ SqlInjectionDemo/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SampleApp
+{
+    class Program
+    {
+        private const string ConnectionString = @"Data Source=.;Initial Catalog=SqlInjectionDemo;Integrated Security=True";
+
+        static void Main()
+        {
+            var con = GetConnection();
+            con.Open();
+            while (true)
+            {
+                #region Get user & pwd
+                Console.WriteLine();
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("Logon and show user details");
+                Console.WriteLine("------------------------------------");
+                Console.Write("Username: ");
+                string user = Console.ReadLine();
+                if (string.IsNullOrEmpty(user)) { break; }
+                Console.Write("PWD: ");
+                string pwd = Console.ReadLine();
+                #endregion
+
+                string sql = "Select * from Users where UserName='" + user + "' and  Password='" + pwd + "';";
+
+                #region Print sql
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine(sql);
+                Console.WriteLine("------------------------------------");
+                Console.ResetColor();
+
+                #endregion
+
+                try
+                {
+                    using (var cmd = GetCommand(con, sql))
+                    {
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            Print(reader);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        static void Print(IDataReader reader)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                sb.Append(reader.GetName(i));
+                sb.Append("\t");
+            }
+            Console.WriteLine(sb.ToString());
+            while (reader.Read())
+            {
+                sb = new StringBuilder();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    sb.Append(reader.GetValue(i));
+                    sb.Append("\t");
+                }
+                Console.WriteLine(sb.ToString());
+            }
+
+        }
+        static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+        static SqlCommand GetCommand(SqlConnection con, string sql)
+        {
+            return new SqlCommand(sql, con);
+
+        }
+
+    }
+}
